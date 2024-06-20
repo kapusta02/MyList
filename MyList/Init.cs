3133f7a91d674b1c24b3c72d098486678f4c7e18
Nikita Stavitskiy <n.saratovtsev@mail.ru>
2024-06-20 15:34:00 +0500

    Implemented the main task, custom writing MyList

--- MyList/Init.cs
+++ MyList/Init.cs
@@ -4,6 +4,15 @@ public class Init
 {
     public void Start()
     {
+        MyList<int> newList = new MyList<int>();
+        newList.Add(2);
+        newList.Add(1);
+        newList.Add(3);
+
+        Console.WriteLine($"Элемент на позиции 1: {newList[1]}");
+        Console.WriteLine($"Общее количество элементов: {newList.Count}");
         
+        newList.Remove(0);
+        Console.WriteLine($"Количество элементов в массиве: {newList.Count}");
     }
 }
---
+++ MyList/MyList.cs
@@ -0,0 +1,42 @@
+namespace MyList;
+
+public class MyList<T>
+{
+    private T[] _items = new T[4];
+    private int _count = 0;
+
+    public void Add(T item)
+    {
+        if (_count == _items.Length)
+            Array.Resize(ref _items, _items.Length * 2);
+        _items[_count] = item;
+        _count++;
+    }
+
+    public T this[int index]
+    {
+        get
+        {
+            if (index >= _count || index < 0)
+            {
+                Console.WriteLine("Index out of range");
+            }
+            return _items[index];
+        }
+    }
+
+    public int Count => _count;
+
+    public void Remove(int index)
+    {
+        if (index >= _count || index < 0)
+            Console.WriteLine("Index out of range");
+        for (int i = index; i<_count-1; i++)
+        {
+            _items[i] = _items[i + 1];
+        }
+
+        _count--;
+        _items[_count] = default(T) ?? throw new InvalidOperationException();
+    }
+}
